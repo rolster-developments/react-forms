cfd50131dd15a6ac73c02a240472e296d50c9a87
ing.dacastillop <ing.dacastillop@gmail.com>
2023-12-05 08:55:25 -0500

    :tada: First commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Rolster React Forms
+
+It implements a set of classes that allow managing the control of states of the input components in React.
+
+## Installation
+
+```
+npm i @rolster/react-forms
+```
+
+## Configuration
+
+You must install the `@rolster/types` to define package data types, which are configured by adding them to the `files` property of the `tsconfig.json` file.
+
+```json
+{
+  "files": ["node_modules/@rolster/types/index.d.ts"]
+}
+```
+
+## Contributing
+
+- Daniel Andrés Castillo Pedroza :rocket:
---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+const plugins = [
+  commonjs(),
+  resolve(),
+  typescript({
+    tsconfig: './tsconfig.app.json',
+    declaration: true,
+    declarationDir: 'dist',
+    include: ['node_modules/@rolster/types/index.d.ts']
+  })
+];
+
+const rollupTs = (file) => {
+  return {
+    input: [`dist/esm/${file}.js`],
+    output: [
+      {
+        file: `dist/cjs/${file}.js`,
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true
+      },
+      {
+        file: `dist/es/${file}.js`,
+        format: 'es',
+        sourcemap: true,
+        inlineDynamicImports: true
+      }
+    ],
+    external: ['@rolster/i18n'],
+    plugins
+  };
+};
+
+export default [rollupTs('index')];
